3af03e7817c94e47dae49d162cf732535af5f24e
Chiranjeet Biswas <chiranjeetbiswas403@gmail.com>
2025-04-22 23:41:22 +0530

    create room

--- base/views.py
+++ base/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 # Create your views here.
 
-rooms=[{'id': 1,'name':'lets learn python'},
-       {'id': 2,'name':'lets learn python'},
-       {'id': 3,'name':'lets learn python'},
-       {'id': 4,'name':'lets learn python'},
-       {'id': 5,'name':'lets learn python'},
-       {'id': 6,'name':'lets learn python'}]
+# rooms=[{'id': 1,'name':'lets learn python'},
+#        {'id': 2,'name':'lets learn python'},
+#        {'id': 3,'name':'lets learn python'},
+#        {'id': 4,'name':'lets learn python'},
+#        {'id': 5,'name':'lets learn python'},
+#        {'id': 6,'name':'lets learn python'}]
 
 
 def home(request):
+    rooms=Room.objects.all()
     context={'rooms':rooms}
     return render(request,'base/home.html',context)
 
 
 def room(request,pk):
-    return render(request,'base/room.html')
+    rooms=Room.objects.get(id=pk)
+    context={'rooms':rooms}
+    return render(request,'base/room.html',context)
+
+def createroom(request):
+    context={}
+    return render(request,'base/room_form.html',context)
