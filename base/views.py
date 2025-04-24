ed7792c1c381a2b61491bc7d3de1bbe537694a4b
Chiranjeet Biswas <chiranjeetbiswas403@gmail.com>
2025-04-24 06:22:10 +0530

    register form

--- base/views.py
+++ base/views.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 from django.contrib import messages
 from django.contrib.auth import authenticate ,login , logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Room , Topic
 from .forms import RoomForm
@@ -19,11 +20,12 @@ from .forms import RoomForm
 
 
 def loginPage(request):
+    page='login'
     if request.user.is_authenticated :
         return redirect('home')
 
     if request.method=='POST':
-        username=request.POST.get('username')
+        username=request.POST.get('username').lower()
         password=request.POST.get('password')
 
         try:
@@ -40,7 +42,24 @@ def loginPage(request):
             messages.error(request,'Username or Password does not match')
 
     # context={'username':username,'password':password}
-    context={}
+    context={'page':page}
+    return render(request,'base/login_register.html',context)
+
+def registerUser(request):
+    # page='register'
+    form=UserCreationForm()
+
+    if request.method == 'POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid() :
+            user=form.save(commit=False)
+            user.username=user.username.lower()
+            user.save()
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'An error genrate during registation')
+    context={'form':form}
     return render(request,'base/login_register.html',context)
 
 def logoutUser(request):
